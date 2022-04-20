1a79b57f76df48bbe029ea026b56ba4e719ecbec
ChengTze-Wu <yaoop3050777@gmail.com>
2022-04-20 16:34:16 +0800

    fix main.js

--- static/js/main.js
+++ static/js/main.js
@@ -7,71 +7,56 @@ const navdate__today_btn = document.querySelector(".nav-date__today_btn");
 
 // merge here
 async function dataControl() {
-
     search__input.classList.remove("error");
     // var
     const locationName = search__input.value;
     const result = search__input.value;
     if (result) {
-        let indexDate = 0;
+        let indexDate = 1;
+        let indexUv = 0;
         const rawData = await getWeatherData(result);
-        const data = dataProcess(rawData, indexDate);
+        const data = dataProcess(rawData, indexDate, indexUv);
         if (data) {
-            search__input.value = "";
-            // data : { temp: "str", speed: "str", humid: "str", pop: "str", uv: "str"}
-            // render function: main-info, second-info
-            console.log(data);
-            // 測試1
-            // 測試2
-            // locationName
-            // nav-date function
-            // addEventListener("click", () => {
-            //     indexDate = 1;
-            //     const data = dataProcess(rawData, indexDate);
-            // });
+            // render
         } else {
             search__input.classList.add("error");
         }
-
     } else {
-      search__input.classList.add("error");
+        search__input.classList.add("error");
     }
-  } else {
-    search__input.classList.add("error");
-  }
 }
 
 // search__icon
 search__icon.addEventListener("click", dataControl);
 search__input.addEventListener(
-  "keydown",
-  (e) => {
-    if (e.keyCode === 13) {
-      dataControl();
-    }
-  },
-  false
+    "keydown",
+    (e) => {
+        if (e.keyCode === 13) {
+            dataControl();
+        }
+    },
+    false
 );
 // nav-date
 //今日天氣
 navdate__today_btn.addEventListener("click", async () => {
-  const result = search__input.value;
-  if (result) {
-    const rawData = await getWeatherData(result);
-    indexDate = 1;
-    indexUv = 0;
-    const data = dataProcess(rawData, indexDate, indexUv);
-    console.log(data);
-  }
+    const result = search__input.value;
+    if (result) {
+        const rawData = await getWeatherData(result);
+        indexDate = 1;
+        indexUv = 0;
+        const data = dataProcess(rawData, indexDate, indexUv);
+        console.log(data);
+    }
 });
 //明日天氣
 navdate__tmr_btn.addEventListener("click", async () => {
-  const result = search__input.value;
-  if (result) {
-    const rawData = await getWeatherData(result);
-    indexDate = 3;
-    indexUv = 1;
-    const data = dataProcess(rawData, indexDate, indexUv);
-    console.log(data);
-  }
+    const result = search__input.value;
+    if (result) {
+        const rawData = await getWeatherData(result);
+        indexDate = 3;
+        indexUv = 1;
+        const data = dataProcess(rawData, indexDate, indexUv);
+        console.log(data);
+    }
 });
