65f0685d595b961095fe878a5935e320a966d027
jchong <rockportereport@gmail.com>
2022-04-20 18:28:59 +0800

    main-into second commit

--- static/js/main-info.js
+++ static/js/main-info.js
@@ -1,10 +1,10 @@
-let sun_img=document.getElementById("sunny");
-let cloud_img=document.getElementById("cloudy");
-let rain_img=document.getElementById("rainy");
-let main_info__temp=document.getElementById("temp");
-let main_info__status=document.getElementById("status");
-
 function renderMainInfo(data){
+    let sun_img=document.getElementById("sunny");
+    let cloud_img=document.getElementById("cloudy");
+    let rain_img=document.getElementById("rainy");
+    let main_info__temp=document.getElementById("temp");
+    let main_info__status=document.getElementById("status");
+    
     let temperature=data.temp;
     main_info__temp.innerHTML=temperature;
 
@@ -13,17 +13,14 @@ function renderMainInfo(data){
     if (chanceOfRain<=30){
         sun_img.style.display="block";
         status="MOSTLY CLEAR";
-        // main_info__status.innerHTML=status;
     }
     else if (chanceOfRain<=70){
         cloud_img.style.display="block";
         status="CLOUDY";
-        // main_info__status.innerHTML=status;
     }
     else{
         rain_img.style.display="block";
         status="RAINY";
-        // main_info__status.innerHTML=status;
     }
     main_info__status.innerHTML=status;
 } 
