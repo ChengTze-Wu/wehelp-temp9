bf8826e18b98b83ae0212733bfe3ea24142116bc
Yin Chuang <blackbest_0110@hotmail.com>
2022-04-20 17:51:21 +0800

    color change

--- static/js/second-info.js
+++ static/js/second-info.js
@@ -1,11 +1,13 @@
 function renderSecondInfo(speed, humid, pop, uv) {
-    let wind = document.querySelector(".second-info__windText");
-    let humidText = document.querySelector(".second-info__humidText");
-    let rainy = document.querySelector(".second-info__popText");
-    let uvRate = document.querySelector(".second-info__uvText");
-
-    wind.innerHTML = speed + " km/h";
-    humidText.innerHTML = humid + "%";
-    rainy.innerHTML = pop + "%";
-    uvRate.innerHTML = uv;
+	let wind = document.querySelector(".second-info__windText");
+	let humidText = document.querySelector(".second-info__humidText");
+	let rainy = document.querySelector(".second-info__popText");
+	let uvRate = document.querySelector(".second-info__uvText");
+	let unit = document.querySelector(".second-info__windText__unit");
+	console.log(unit);
+	wind.innerHTML = speed;
+	unit.innerHTML = "km/h";
+	humidText.innerHTML = humid + "%";
+	rainy.innerHTML = pop + "%";
+	uvRate.innerHTML = uv;
 }
