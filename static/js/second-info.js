41459d43fdc48761be073a48f1e41b381bb24405
ChengTze-Wu <yaoop3050777@gmail.com>
2022-04-20 17:09:42 +0800

    fix

--- static/js/second-info.js
+++ static/js/second-info.js
@@ -1,11 +1,11 @@
 function renderSecondInfo(speed, humid, pop, uv) {
     let wind = document.querySelector(".second-info__windText");
-    let humid = document.querySelector(".second-info__humidText");
+    let humidText = document.querySelector(".second-info__humidText");
     let rainy = document.querySelector(".second-info__popText");
     let uvRate = document.querySelector(".second-info__uvText");
 
     wind.innerHTML = speed + " km/h";
-    this.humid.innerHTML = humid + "%";
+    humidText.innerHTML = humid + "%";
     rainy.innerHTML = pop + "%";
     uvRate.innerHTML = uv;
 }
